152df987d4564b787f96bb4bb5398203829e49f9
Diksha_ Phogat <phogatdiksha463@gmail.com>
2026-05-28 20:36:41 +0530

    fixed api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5001/api' });
-
+const API = axios.create({ 
+  baseURL: "https://applywise-3dp9.onrender.com/api"
+});
 // Attach JWT token to every request
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
